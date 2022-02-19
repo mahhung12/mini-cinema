7ac95587192c959c3f3cc43a6c9e26bd32537d8e
mhung286 <mahhungg.12@gmail.com>
2022-02-19 17:05:17 +0700

    update logo account.js file

--- src/pages/account/account.js
+++ src/pages/account/account.js
@@ -6,64 +6,41 @@ import netflix from "../../assests/imgs/netflix.png";
 export default function account() {
     return (
         <div className="landing-container">
-            <Link to="/">
-                <img
-                    className="icon"
-                    // src="../../../public/netflix.png"
-                    src={netflix}
-                    alt="landing"
-                />
-            </Link>
+            <div className="wrap-logo">
+                <Link to="/">
+                    <img className="icon" src={netflix} alt="landing" />
+                </Link>
+            </div>
             <div className="landing-container__center">
                 <span className="text">Who's watching?</span>
                 <div className="members">
                     <Link to="/home" className="member">
                         <div className="avatar">
-                            <img
-                                src={netflix}
-                                alt="avatar"
-                                srcSet=""
-                            />
+                            <img src={netflix} alt="avatar" srcSet="" />
                             <p className="name">Account 1</p>
                         </div>
                     </Link>
                     <Link to="/home" className="member">
                         <div className="avatar">
-                            <img
-                                src={netflix}
-                                alt="avatar"
-                                srcSet=""
-                            />
+                            <img src={netflix} alt="avatar" srcSet="" />
                             <p className="name">Account 2</p>
                         </div>
                     </Link>
                     <Link to="/home" className="member">
                         <div className="avatar">
-                            <img
-                                src={netflix}
-                                alt="avatar"
-                                srcSet=""
-                            />
+                            <img src={netflix} alt="avatar" srcSet="" />
                             <p className="name">Account 3</p>
                         </div>
                     </Link>
                     <Link to="/home" className="member">
                         <div className="avatar">
-                            <img
-                                src={netflix}
-                                alt="avatar"
-                                srcSet=""
-                            />
+                            <img src={netflix} alt="avatar" srcSet="" />
                             <p className="name">Account 4</p>
                         </div>
                     </Link>
                     <Link to="/home" className="member">
                         <div className="avatar">
-                            <img
-                                src={netflix}
-                                alt="avatar"
-                                srcSet=""
-                            />
+                            <img src={netflix} alt="avatar" srcSet="" />
                             <p className="name">Account 5</p>
                         </div>
                     </Link>
